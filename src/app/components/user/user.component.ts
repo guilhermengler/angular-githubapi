3d9cae76e6ef162247656e398c816793e68a9f65
Guilherme Engler <g.eengler@gmail.com>
2019-01-26 15:51:36 -0200

    Add Oberservations

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Input, ChangeDetectionStrategy } from '@angular/core
 import { User } from 'src/app/models/user.model';
 import { GithubService } from 'src/app/services/github.service';
 import { AppComponent } from 'src/app/app.component';
+import { stringify } from '@angular/core/src/render3/util';
 
 @Component({
   selector: 'app-user',
@@ -12,12 +13,19 @@ import { AppComponent } from 'src/app/app.component';
 export class UserComponent implements OnInit {
   @Input()
   user: User;
-
-  ok;
-
-  constructor(private githubService: GithubService, private appComponent: AppComponent) { }
+  localStorage: boolean = false;
+  fieldObservations: string = '';
+  
+  constructor(private githubService: GithubService) { }
 
   ngOnInit() {
-    this.ok = this.appComponent.user;
+  }
+
+  getLocalStorage(name: string) {
+    return localStorage.getItem(name);
+  }
+
+  setLocalStorage(name: string, text: string) {
+    localStorage.setItem(name, text)
   }
 }
